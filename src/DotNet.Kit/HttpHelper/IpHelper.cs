806bb6e8652fc41ad909dda38e233e2506c31a89
阿星Plus <hackxing@foxmail.com>
2018-04-18 13:49:19 +0800

    :zap: Add XmlHelper、IpHelper,Update TimeHelper

---
+++ src/DotNet.Kit/HttpHelper/IpHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Web;
+
+namespace DotNet.Kit
+{
+    public class IpHelper
+    {
+        /// <summary>
+        /// 获得用户IP
+        /// </summary>
+        /// <returns></returns>
+        public static string GetUserIp()
+        {
+            string ip;
+            string[] temp;
+            bool isErr = false;
+            if (HttpContext.Current.Request.ServerVariables["HTTP_X_ForWARDED_For"] == null)
+            {
+                ip = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"].ToString();
+            }
+            else
+            {
+                ip = HttpContext.Current.Request.ServerVariables["HTTP_X_ForWARDED_For"].ToString();
+            }
+
+            if (ip.Length > 15)
+            {
+                isErr = true;
+            }
+            else
+            {
+                temp = ip.Split('.');
+                if (temp.Length == 4)
+                {
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (temp[i].Length > 3)
+                        {
+                            isErr = true;
+                        }
+                    }
+                }
+                else
+                {
+                    isErr = true;
+                }
+            }
+
+            if (isErr)
+            {
+                return "1.1.1.1";
+            }
+            else
+            {
+                return ip;
+            }
+        }
+    }
+}
