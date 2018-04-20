88df6fb8397dca25631172df02879f9235dac2d9
阿星Plus <hackxing@foxmail.com>
2018-04-20 16:11:58 +0800

    :zap: Add CacheHelper、RandomHelper

---
+++ src/DotNet.Kit/CacheHelper/CacheHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace DotNet.Kit
+{
+    public class CacheHelper
+    {
+        /// <summary>
+        /// 获取数据缓存
+        /// </summary>
+        /// <param name="cacheKey">键</param>
+        /// <returns></returns>
+        public static object GetCache(string cacheKey)
+        {
+            var cache = HttpRuntime.Cache;
+            return cache[cacheKey];
+        }
+
+        /// <summary>
+        /// 设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="value"></param>
+        public static void SetCache(string cacheKey, object value)
+        {
+            var cache = HttpRuntime.Cache;
+            cache.Insert(cacheKey, value);
+        }
+
+        /// <summary>
+        /// 设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="value"></param>
+        /// <param name="timeout"></param>
+        public static void SetCache(string cacheKey, object value, TimeSpan timeout)
+        {
+            var cache = HttpRuntime.Cache;
+            cache.Insert(cacheKey, value, null, DateTime.MaxValue, timeout, CacheItemPriority.NotRemovable, null);
+        }
+
+        /// <summary>
+        /// 设置数据缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        /// <param name="value"></param>
+        /// <param name="absoluteExpiration"></param>
+        /// <param name="slidingExpiration"></param>
+        public static void SetCache(string cacheKey, object value, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+        {
+            var cache = HttpRuntime.Cache;
+            cache.Insert(cacheKey, value, null, absoluteExpiration, slidingExpiration);
+        }
+
+        /// <summary>
+        /// 移除指定数据缓存
+        /// </summary>
+        /// <param name="cacheKey"></param>
+        public static void RemoveCache(string cacheKey)
+        {
+            var cache = HttpRuntime.Cache;
+            cache.Remove(cacheKey);
+        }
+
+        /// <summary>
+        /// 移除全部缓存
+        /// </summary>
+        public static void RemoveCache()
+        {
+            var cache = HttpRuntime.Cache;
+            var cacheEnum = cache.GetEnumerator();
+            while (cacheEnum.MoveNext())
+            {
+                cache.Remove(cacheEnum.Key.ToString());
+            }
+        }
+    }
+}
---
+++ src/DotNet.Kit/RandomHelper/RandomHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace DotNet.Kit
+{
+    public class RandomHelper
+    {
+        private int rep = 0;
+
+        private Random _random;
+        public RandomHelper()
+        {
+            _random = new Random();
+        }
+
+        /// <summary>
+        /// 生成一个指定范围的随机整数，该随机数范围包括最小值，但不包括最大值
+        /// </summary>
+        /// <param name="minNum">最小值</param>
+        /// <param name="maxNum">最大值</param>
+        /// <returns></returns>
+        public int GetRandomInt(int minNum,int maxNum)
+        {
+            return _random.Next(minNum, maxNum);
+        }
+
+        /// <summary>
+        /// 生成一个0.0到1.0的随机小数
+        /// </summary>
+        /// <returns></returns>
+        public double GetRandomDouble()
+        {
+            return _random.NextDouble();
+        }
+
+        /// <summary>
+        /// 对一个数组进行随机排序
+        /// </summary>
+        /// <typeparam name="T">数组的类型</typeparam>
+        /// <param name="arr">需要随机排序的数组</param>
+        public void GetRandomArray<T>(T[] arr)
+        {
+            int count = arr.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                int targetIndex1 = GetRandomInt(0, arr.Length);
+                int targetIndex2 = GetRandomInt(0, arr.Length);
+
+                T temp;
+
+                temp = arr[targetIndex1];
+                arr[targetIndex1] = arr[targetIndex2];
+                arr[targetIndex2] = temp;
+            }
+        }
+
+        /// <summary>
+        /// 生成随机不重复数字字符串    
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public string GetRandomInt(int length)
+        {
+            var str = string.Empty;
+            var num = DateTime.Now.Ticks + rep;
+            rep++;
+
+            var random = new Random(((int)(((ulong)num) & 0xffffffffL)) | ((int)(num >> rep)));
+            for (int i = 0; i < length; i++)
+            {
+                int randomNum = random.Next();
+                str = str + ((char)(0x30 + ((ushort)(randomNum % 10)))).ToString();
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 生成随机字符串（数字和字母混和）  
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public string GetRandomString(int length)
+        {
+            var str = string.Empty;
+            var num = DateTime.Now.Ticks + rep;
+            rep++;
+
+            var random = new Random(((int)(((ulong)num) & 0xffffffffL)) | ((int)(num >> rep)));
+            for (int i = 0; i < length; i++)
+            {
+                char ch;
+                int randomNum = random.Next();
+                if (randomNum % 2 == 0)
+                {
+                    ch = (char)(0x30 + ((ushort)(randomNum % 10)));
+                }
+                else
+                {
+                    ch = (char)(0x41 + ((ushort)(randomNum % 0x1a)));
+                }
+                str = str + ch.ToString();
+            }
+            return str;
+        }
+    }
+}
